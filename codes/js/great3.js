2b9732dfc4482faf911283907c1486e913c9d661
grey-4 <sarath.s2022ece@sece.ac.in>
2024-10-10 09:43:04 +0530

    Clean

--- codes/js/great3.js
+++
@@ -1,6 +0,0 @@
-let a = 19;
-let b = 11;
-let c = 10;
-let e = a>c?a:c
-let d = e>b?e:b
-console.log(d)
--- codes/js/hihello.js
+++
@@ -1,6 +0,0 @@
-let a = 15;
-
-let str1 = (a%3==0)?"hi":"";
-let str2 = (a%5==0)?"hello":"";
-
-console.log(str1+str2);
